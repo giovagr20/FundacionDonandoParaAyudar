bd7eb8de1f0fa08a1e462fb47f6e4dd1804032de
giovagr20 <giovannyg32@gmail.com>
2020-08-04 08:05:27 -0500

    Se añade arquitectura de trabajo web

---
+++ FundacionDonandoParaAyudar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FundacionDonandoParaAyudar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
